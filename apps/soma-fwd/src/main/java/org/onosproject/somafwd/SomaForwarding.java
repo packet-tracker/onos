bd160a9685296bc1be25e7dfe3675e4058ca4428
lminjae <lminjae@lminjaeui-MacBook-Pro.local>
2015-11-14 17:53:32 +0900

    Init soma-fwd
    add funtions for honeypot
    
    please fix Honeypot's MacAddress
    
    addBlackList on ICMP pkt detected

---
+++ apps/soma-fwd/src/main/java/org/onosproject/somafwd/SomaForwarding.java
@@ -0,0 +1,506 @@
+/*
+ * Copyright 2014-2015 Open Networking Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onosproject.somafwd;
+
+import com.google.common.collect.ImmutableSet;
+import org.apache.felix.scr.annotations.Activate;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Deactivate;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.ReferenceCardinality;
+import org.onlab.packet.Ethernet;
+import org.onlab.packet.IPv4;
+import org.onlab.packet.MacAddress;
+import org.onosproject.core.ApplicationId;
+import org.onosproject.core.CoreService;
+import org.onosproject.event.Event;
+import org.onosproject.net.ConnectPoint;
+import org.onosproject.net.DeviceId;
+import org.onosproject.net.Host;
+import org.onosproject.net.HostId;
+import org.onosproject.net.Link;
+import org.onosproject.net.Path;
+import org.onosproject.net.PortNumber;
+import org.onosproject.net.flow.DefaultTrafficSelector;
+import org.onosproject.net.flow.DefaultTrafficTreatment;
+import org.onosproject.net.flow.FlowEntry;
+import org.onosproject.net.flow.FlowRule;
+import org.onosproject.net.flow.FlowRuleService;
+import org.onosproject.net.flow.TrafficSelector;
+import org.onosproject.net.flow.TrafficTreatment;
+import org.onosproject.net.flow.criteria.Criterion;
+import org.onosproject.net.flow.criteria.EthCriterion;
+import org.onosproject.net.flow.instructions.Instruction;
+import org.onosproject.net.flow.instructions.Instructions;
+import org.onosproject.net.flowobjective.DefaultForwardingObjective;
+import org.onosproject.net.flowobjective.FlowObjectiveService;
+import org.onosproject.net.flowobjective.ForwardingObjective;
+import org.onosproject.net.host.HostService;
+import org.onosproject.net.link.LinkEvent;
+import org.onosproject.net.packet.InboundPacket;
+import org.onosproject.net.packet.PacketContext;
+import org.onosproject.net.packet.PacketPriority;
+import org.onosproject.net.packet.PacketProcessor;
+import org.onosproject.net.packet.PacketService;
+import org.onosproject.net.topology.TopologyEvent;
+import org.onosproject.net.topology.TopologyListener;
+import org.onosproject.net.topology.TopologyService;
+import org.slf4j.Logger;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * Soma forwarding application.
+ */
+@Component(immediate = true)
+public class SomaForwarding {
+
+    private static final int DEFAULT_TIMEOUT = 10;
+    private static final int DEFAULT_PRIORITY = 10;
+
+    // XXX
+    private static final MacAddress HONEYPOT_MACADDRESS = MacAddress.valueOf("00:00:00:00:00:01");
+
+    private final Logger log = getLogger(getClass());
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected TopologyService topologyService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected PacketService packetService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected HostService hostService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected FlowRuleService flowRuleService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected FlowObjectiveService flowObjectiveService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected CoreService coreService;
+
+    private ReactivePacketProcessor processor = new ReactivePacketProcessor();
+
+    private ApplicationId appId;
+
+    private final TopologyListener topologyListener = new InternalTopologyListener();
+
+    private List<MacAddress> blackList = new ArrayList<MacAddress>();
+
+
+    @Activate
+    public void activate() {
+        appId = coreService.registerApplication("org.onosproject.somafwd");
+
+        packetService.addProcessor(processor, PacketProcessor.director(2));
+        topologyService.addListener(topologyListener);
+        requestIntercepts();
+
+        log.info("Started", appId.id());
+    }
+
+    @Deactivate
+    public void deactivate() {
+        withdrawIntercepts();
+        flowRuleService.removeFlowRulesById(appId);
+        packetService.removeProcessor(processor);
+        topologyService.removeListener(topologyListener);
+        processor = null;
+        log.info("Stopped");
+    }
+
+    /**
+     * Request packet in via packet service.
+     */
+    private void requestIntercepts() {
+        TrafficSelector.Builder selector = DefaultTrafficSelector.builder();
+        selector.matchEthType(Ethernet.TYPE_IPV4);
+        packetService.requestPackets(selector.build(), PacketPriority.REACTIVE, appId);
+        selector.matchEthType(Ethernet.TYPE_ARP);
+        packetService.requestPackets(selector.build(), PacketPriority.REACTIVE, appId);
+        selector.matchEthType(Ethernet.TYPE_IPV6);
+        packetService.requestPackets(selector.build(), PacketPriority.REACTIVE, appId);
+    }
+
+    /**
+     * Cancel request for packet in via packet service.
+     */
+    private void withdrawIntercepts() {
+        TrafficSelector.Builder selector = DefaultTrafficSelector.builder();
+        selector.matchEthType(Ethernet.TYPE_IPV4);
+        packetService.cancelPackets(selector.build(), PacketPriority.REACTIVE, appId);
+        selector.matchEthType(Ethernet.TYPE_ARP);
+        packetService.cancelPackets(selector.build(), PacketPriority.REACTIVE, appId);
+        selector.matchEthType(Ethernet.TYPE_IPV6);
+        packetService.cancelPackets(selector.build(), PacketPriority.REACTIVE, appId);
+    }
+
+    private boolean onBlackList(MacAddress addr) {
+        return blackList.contains(addr);
+    }
+
+    private MacAddress chkOnBlackList(Ethernet ethPkt) {
+        if (onBlackList(ethPkt.getSourceMAC())) {
+            log.warn("Forwarding packet from {} to {}", ethPkt.getSourceMAC(), HONEYPOT_MACADDRESS);
+            return HONEYPOT_MACADDRESS;
+        }
+        log.info("Forwarding packer from {} to {}", ethPkt.getSourceMAC(), ethPkt.getDestinationMAC());
+        return ethPkt.getDestinationMAC();
+    }
+
+    private void addBlackList(MacAddress addr) {
+        if (!onBlackList(addr) && addr != HONEYPOT_MACADDRESS) {
+            blackList.add(addr);
+        }
+    }
+
+    /**
+     * Packet processor responsible for forwarding packets along their paths.
+     */
+    private class ReactivePacketProcessor implements PacketProcessor {
+
+        @Override
+        public void process(PacketContext context) {
+            // Stop processing if the packet has been handled, since we
+            // can't do any more to it.
+
+            if (context.isHandled()) {
+                return;
+            }
+
+            InboundPacket pkt = context.inPacket();
+            Ethernet ethPkt = pkt.parsed();
+
+            if (ethPkt == null) {
+                return;
+            }
+
+            // Bail if this is deemed to be a control packet.
+            if (isControlPacket(ethPkt)) {
+                return;
+            }
+
+            if (ethPkt.getEtherType() == Ethernet.TYPE_IPV4 &&
+               ((IPv4) ethPkt.getPayload()).getProtocol() == IPv4.PROTOCOL_ICMP) {
+                addBlackList(ethPkt.getSourceMAC());
+            }
+
+            HostId id = HostId.hostId(chkOnBlackList(ethPkt));
+
+            // Do not process link-local addresses in any way.
+            if (id.mac().isLinkLocal()) {
+                return;
+            }
+
+            // Do we know who this is for? If not, flood and bail.
+            Host dst = hostService.getHost(id);
+            if (dst == null) {
+                flood(context);
+                return;
+            }
+
+            // Are we on an edge switch that our destination is on? If so,
+            // simply forward out to the destination and bail.
+            if (pkt.receivedFrom().deviceId().equals(dst.location().deviceId())) {
+                if (!context.inPacket().receivedFrom().port().equals(dst.location().port())) {
+                    installRule(context, dst.location().port());
+                }
+                return;
+            }
+
+            // Otherwise, get a set of paths that lead from here to the
+            // destination edge switch.
+            Set<Path> paths =
+                    topologyService.getPaths(topologyService.currentTopology(),
+                                             pkt.receivedFrom().deviceId(),
+                                             dst.location().deviceId());
+            if (paths.isEmpty()) {
+                // If there are no paths, flood and bail.
+                flood(context);
+                return;
+            }
+
+            // Otherwise, pick a path that does not lead back to where we
+            // came from; if no such path, flood and bail.
+            Path path = pickForwardPathIfPossible(paths, pkt.receivedFrom().port());
+            if (path == null) {
+                log.warn("Don't know where to go from here {} for {} -> {}",
+                         pkt.receivedFrom(), ethPkt.getSourceMAC(), ethPkt.getDestinationMAC());
+                flood(context);
+                return;
+            }
+
+            // Otherwise forward and be done with it.
+            installRule(context, path.src().port());
+        }
+
+    }
+
+    // Indicates whether this is a control packet, e.g. LLDP, BDDP
+    private boolean isControlPacket(Ethernet eth) {
+        short type = eth.getEtherType();
+        return type == Ethernet.TYPE_LLDP || type == Ethernet.TYPE_BSN;
+    }
+
+    // Selects a path from the given set that does not lead back to the
+    // specified port if possible.
+    private Path pickForwardPathIfPossible(Set<Path> paths, PortNumber notToPort) {
+        Path lastPath = null;
+        for (Path path : paths) {
+            lastPath = path;
+            if (!path.src().port().equals(notToPort)) {
+                return path;
+            }
+        }
+        return lastPath;
+    }
+
+    // Floods the specified packet if permissible.
+    private void flood(PacketContext context) {
+        if (topologyService.isBroadcastPoint(topologyService.currentTopology(),
+                                             context.inPacket().receivedFrom())) {
+            packetOut(context, PortNumber.FLOOD);
+        } else {
+            context.block();
+        }
+    }
+
+    // Sends a packet out the specified port.
+    private void packetOut(PacketContext context, PortNumber portNumber) {
+        context.treatmentBuilder().setOutput(portNumber);
+        context.send();
+    }
+
+    // Install a rule forwarding the packet to the specified port.
+    private void installRule(PacketContext context, PortNumber portNumber) {
+        //
+        // We don't support (yet) buffer IDs in the Flow Service so
+        // packet out first.
+        //
+        Ethernet inPkt = context.inPacket().parsed();
+        TrafficSelector.Builder selectorBuilder = DefaultTrafficSelector.builder();
+
+        // If ARP packet than forward directly to output port
+        if (inPkt.getEtherType() == Ethernet.TYPE_ARP) {
+            packetOut(context, portNumber);
+            return;
+        }
+
+        //
+        // Create flows with default matching and include configured fields
+        //
+        selectorBuilder.matchInPort(context.inPacket().receivedFrom().port())
+                .matchEthSrc(inPkt.getSourceMAC())
+                .matchEthDst(inPkt.getDestinationMAC());
+
+        TrafficTreatment treatment = DefaultTrafficTreatment.builder()
+                .setOutput(portNumber)
+                .build();
+
+        ForwardingObjective forwardingObjective = DefaultForwardingObjective.builder()
+                .withSelector(selectorBuilder.build())
+                .withTreatment(treatment)
+                .withPriority(DEFAULT_PRIORITY)
+                .withFlag(ForwardingObjective.Flag.VERSATILE)
+                .fromApp(appId)
+                .makeTemporary(DEFAULT_TIMEOUT)
+                .add();
+
+        flowObjectiveService.forward(context.inPacket().receivedFrom().deviceId(),
+                                     forwardingObjective);
+
+        //
+        // Send packet direction on the appropriate port
+        //
+        packetOut(context, portNumber);
+    }
+
+    private class InternalTopologyListener implements TopologyListener {
+        @Override
+        public void event(TopologyEvent event) {
+            List<Event> reasons = event.reasons();
+            if (reasons != null) {
+                reasons.forEach(re -> {
+                    if (re instanceof LinkEvent) {
+                        LinkEvent le = (LinkEvent) re;
+                        if (le.type() == LinkEvent.Type.LINK_REMOVED) {
+                            fixBlackhole(le.subject().src());
+                        }
+                    }
+                });
+            }
+        }
+    }
+
+    private void fixBlackhole(ConnectPoint egress) {
+        Set<FlowEntry> rules = getFlowRulesFrom(egress);
+        Set<SrcDstPair> pairs = findSrcDstPairs(rules);
+
+        Map<DeviceId, Set<Path>> srcPaths = new HashMap<>();
+
+        for (SrcDstPair sd : pairs) {
+            // get the edge deviceID for the src host
+            Host srcHost = hostService.getHost(HostId.hostId(sd.src));
+            Host dstHost = hostService.getHost(HostId.hostId(sd.dst));
+            if (srcHost != null && dstHost != null) {
+                DeviceId srcId = srcHost.location().deviceId();
+                DeviceId dstId = dstHost.location().deviceId();
+                log.trace("SRC ID is " + srcId + ", DST ID is " + dstId);
+
+                cleanFlowRules(sd, egress.deviceId());
+
+                Set<Path> shortestPaths = srcPaths.get(srcId);
+                if (shortestPaths == null) {
+                    shortestPaths = topologyService.getPaths(topologyService.currentTopology(),
+                            egress.deviceId(), srcId);
+                    srcPaths.put(srcId, shortestPaths);
+                }
+                backTrackBadNodes(shortestPaths, dstId, sd);
+            }
+        }
+    }
+
+    // Backtracks from link down event to remove flows that lead to blackhole
+    private void backTrackBadNodes(Set<Path> shortestPaths, DeviceId dstId, SrcDstPair sd) {
+        for (Path p : shortestPaths) {
+            List<Link> pathLinks = p.links();
+            for (int i = 0; i < pathLinks.size(); i = i + 1) {
+                Link curLink = pathLinks.get(i);
+                DeviceId curDevice = curLink.src().deviceId();
+
+                // skipping the first link because this link's src has already been pruned beforehand
+                if (i != 0) {
+                    cleanFlowRules(sd, curDevice);
+                }
+
+                Set<Path> pathsFromCurDevice =
+                        topologyService.getPaths(topologyService.currentTopology(),
+                                                 curDevice, dstId);
+                if (pickForwardPathIfPossible(pathsFromCurDevice, curLink.src().port()) != null) {
+                    break;
+                } else {
+                    if (i + 1 == pathLinks.size()) {
+                        cleanFlowRules(sd, curLink.dst().deviceId());
+                    }
+                }
+            }
+        }
+    }
+
+    // Removes flow rules off specified device with specific SrcDstPair
+    private void cleanFlowRules(SrcDstPair pair, DeviceId id) {
+        log.trace("Searching for flow rules to remove from: " + id);
+        log.trace("Removing flows w/ SRC=" + pair.src + ", DST=" + pair.dst);
+        for (FlowEntry r : flowRuleService.getFlowEntries(id)) {
+            boolean matchesSrc = false, matchesDst = false;
+            for (Instruction i : r.treatment().allInstructions()) {
+                if (i.type() == Instruction.Type.OUTPUT) {
+                    // if the flow has matching src and dst
+                    for (Criterion cr : r.selector().criteria()) {
+                        if (cr.type() == Criterion.Type.ETH_DST) {
+                            if (((EthCriterion) cr).mac().equals(pair.dst)) {
+                                matchesDst = true;
+                            }
+                        } else if (cr.type() == Criterion.Type.ETH_SRC) {
+                            if (((EthCriterion) cr).mac().equals(pair.src)) {
+                                matchesSrc = true;
+                            }
+                        }
+                    }
+                }
+            }
+            if (matchesDst && matchesSrc) {
+                log.trace("Removed flow rule from device: " + id);
+                flowRuleService.removeFlowRules((FlowRule) r);
+            }
+        }
+
+    }
+
+    // Returns a set of src/dst MAC pairs extracted from the specified set of flow entries
+    private Set<SrcDstPair> findSrcDstPairs(Set<FlowEntry> rules) {
+        ImmutableSet.Builder<SrcDstPair> builder = ImmutableSet.builder();
+        for (FlowEntry r : rules) {
+            MacAddress src = null, dst = null;
+            for (Criterion cr : r.selector().criteria()) {
+                if (cr.type() == Criterion.Type.ETH_DST) {
+                    dst = ((EthCriterion) cr).mac();
+                } else if (cr.type() == Criterion.Type.ETH_SRC) {
+                    src = ((EthCriterion) cr).mac();
+                }
+            }
+            builder.add(new SrcDstPair(src, dst));
+        }
+        return builder.build();
+    }
+
+    // Returns set of flow entries which were created by this application and
+    // which egress from the specified connection port
+    private Set<FlowEntry> getFlowRulesFrom(ConnectPoint egress) {
+        ImmutableSet.Builder<FlowEntry> builder = ImmutableSet.builder();
+        flowRuleService.getFlowEntries(egress.deviceId()).forEach(r -> {
+            if (r.appId() == appId.id()) {
+                r.treatment().allInstructions().forEach(i -> {
+                    if (i.type() == Instruction.Type.OUTPUT) {
+                        if (((Instructions.OutputInstruction) i).port().equals(egress.port())) {
+                            builder.add(r);
+                        }
+                    }
+                });
+            }
+        });
+
+        return builder.build();
+    }
+
+    // Wrapper class for a source and destination pair of MAC addresses
+    private final class SrcDstPair {
+        final MacAddress src;
+        final MacAddress dst;
+
+        private SrcDstPair(MacAddress src, MacAddress dst) {
+            this.src = src;
+            this.dst = dst;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            SrcDstPair that = (SrcDstPair) o;
+            return Objects.equals(src, that.src) &&
+                    Objects.equals(dst, that.dst);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(src, dst);
+        }
+    }
+}
